9fcf811db99c462dae5a40fdd55ab975ff7a2c4f
nagae-memooff <169192@gmail.com>
2021-06-21 00:00:17 +0800

    修改启动脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # docker build -t bestwu/qq:office -f office/Dockerfile .
-docker build -t bestwu/qq:im -f im/Dockerfile .
+docker build -t nagae-memooff/qq:9.1.8 -f im/Dockerfile .
 # docker build -t bestwu/qq:light -t bestwu/qq -f im.light/Dockerfile .
 # docker build -t bestwu/qq:eim -f eim/Dockerfile .
--- im/run.sh
+++ im/run.sh
@@ -4,12 +4,15 @@ docker run -d --name qq \
 --device /dev/snd --ipc=host \
 -v "/home/nagae-memooff/文档/Tencent Files":/TencentFiles \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
+-v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
 -e XMODIFIERS=@im=ibus \
 -e QT_IM_MODULE=ibus \
 -e GTK_IM_MODULE=ibus \
 -e DISPLAY=unix$DISPLAY \
 -e AUDIO_GID=`getent group audio | cut -d: -f3` \
 -e VIDEO_GID=`getent group video | cut -d: -f3` \
+-e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+-e XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR} \
 -e GID=`id -g` \
 -e UID=`id -u` \
 nagae-memooff/qq:9.1.8
