562349989b02ef6450969b1f6f6f7cb46c212be1
nagae-memooff <169192@gmail.com>
2021-06-21 00:43:22 +0800

    修改高分辨率脚本

--- hidpi/run.sh
+++ hidpi/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker cp logpixels.reg qq:/
-docker exec -it qq  su qq -c 'WINEPREFIX=/home/qq/.deepinwine/Deepin-$APP deepin-wine regedit /logpixels.reg'
+docker exec -it qq chown qq:qq /logpixels.reg
+docker exec -it qq su qq -c 'WINEPREFIX=/home/qq/.deepinwine/Deepin-$APP deepin-wine regedit /logpixels.reg'
