c7b827a3bfe0dbc1feb6489fb1f3694861df71a8
nagae-memooff <169192@gmail.com>
2021-06-22 23:46:27 +0800

    update

--- hidpi/run.sh
+++ hidpi/run.sh
@@ -3,3 +3,4 @@
 docker cp logpixels.reg qq:/
 docker exec -it qq chown qq:qq /logpixels.reg
 docker exec -it qq su qq -c 'WINEPREFIX=/home/qq/.deepinwine/Deepin-$APP deepin-wine regedit /logpixels.reg'
+docker exec -it qq rm /logpixels.reg
